133b2c5b27b09f8490e9c567f7b90025a4db1248
artvvr <artur.metelskiy@gmail.com>
2021-11-10 14:07:30 +0300

    fix: 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,15 +4,29 @@ const path = require('path')
 let pathFiles = path.join(__dirname, 'files')
 let pathCopyFiles = path.join(__dirname, 'files-copy')
 
-fs.readdir(pathFiles, { withFileTypes: true }, function(err, files) {
-    if (err) throw err
-    for (const file of files) {
-        fs.mkdir('04-copy-directory/files-copy', { recursive: true }, (err) => {
-            if (err) throw err
-            fs.copyFile(path.join(pathFiles, file.name), path.join(pathCopyFiles, file.name), (err) => {
+function copyFile() {
+    fs.readdir(pathFiles, { withFileTypes: true }, function(err, files) {
+        if (err) throw err
+        for (const file of files) {
+            fs.mkdir('04-copy-directory/files-copy', { recursive: true }, (err) => {
                 if (err) throw err
-                console.log(`The file ${file.name} was successfully copied to the directory: ${path.basename(pathCopyFiles)}.`)
+                fs.copyFile(path.join(pathFiles, file.name), path.join(pathCopyFiles, file.name), (err) => {
+                    if (err) throw err
+                    console.log(`The file ${file.name} was successfully copied to the directory: ${path.basename(pathCopyFiles)}.`)
+                })
             })
+        }
+    })
+}
+
+function del() {
+    if (pathCopyFiles) {
+        fs.rmdir(pathCopyFiles, { recursive: true}, () => {
+            copyFile()
         })
+    } else {
+        copyFile()
     }
-})
+}
+
+del()
